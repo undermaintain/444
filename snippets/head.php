95d01e04a08cdbed8e837b0830234731ffb33150
archivistundermaintain <1037347010@qq.com>
2017-03-12 12:35:18 -0700

    added css link

--- snippets/head.php
+++ snippets/head.php
@@ -1,6 +1,16 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <title><?php echo $front_matter['page_title']; ?></title>
   <script src="<?php echo $front_matter['www']; ?>/dependencies/jquery-3.1.1.js"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
+
+  
+</html>
